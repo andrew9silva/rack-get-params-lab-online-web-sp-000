5341c625de6628362710790e10fda65a2c54a5ad
Andrew Silva <andrewsilva9@gmail.com>
2019-10-15 01:06:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,8 @@ class Application
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "#{search_term}"
+      else
+        resp.write "We don't have that item"
     else
       resp.write "Path Not Found"
     end
